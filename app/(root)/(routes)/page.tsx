51bc8f4644d932083594e3fdb142555f4a3a6421
PhucPTiT <phucndah2002@gmail.com>
2023-08-08 14:57:12 +0700

    push categpry filter

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,12 +1,20 @@
 import { Menu } from 'lucide-react'
 import { UserButton } from '@clerk/nextjs'
 import SearchInput from '@/components/search-input'
-export default function RootPage() {
-  return (
+import prismadb from '@/lib/prismadb'
+import Categories from '@/components/categories'
+
+const RootPage = async () => {
+  const categories = await prismadb.category.findMany()
+
+  return ( 
     <div>
       <div className='h-full p-4 space-y-2'>
         <SearchInput/>
+        <Categories data = {categories}/>
       </div>
     </div>
-  )
+   );
 }
+ 
+export default RootPage;
